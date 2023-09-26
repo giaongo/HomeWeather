d47a57397ffe19a33bc6f8e7e9ded9a9524279c8
Giao Ngo <giaongo.97@gmail.com>
2023-09-26 18:00:54 +0300

    start and bind BluetoothLEService class in MainActivity.kt

--- HomeWeather/app/src/main/java/fi/metropolia/homeweather/ui/views/MainActivity.kt
+++ HomeWeather/app/src/main/java/fi/metropolia/homeweather/ui/views/MainActivity.kt
@@ -1,8 +1,14 @@
 package fi.metropolia.homeweather.ui.views
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -47,6 +53,8 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import fi.metropolia.homeweather.R
 import fi.metropolia.homeweather.ui.theme.HomeWeatherTheme
+import fi.metropolia.homeweather.util.service.BluetoothLEService
+import fi.metropolia.homeweather.viewmodels.AppViewModel
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -54,15 +62,41 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Start and bind Bluetooth Service
+        Intent(applicationContext,BluetoothLEService::class.java).apply {
+            startForegroundService(this)
+            bindService(this, serviceConnect, Context.BIND_AUTO_CREATE)
+        }
+
         setContent {
             HomeWeatherTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainApp()
+                    MainApp(appViewModel)
                 }
             }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Unbind bluetooth service
+        unbindService(serviceConnect)
+    }
+
+    /** Defines callbacks for service binding, passed to bindService().  */
+    private val serviceConnect: ServiceConnection = object : ServiceConnection {
+        override fun onServiceConnected(className: ComponentName?, service: IBinder?) {
+            // bound to LocalService, cast the IBinder and get LocalService instance.
+            val binder = service as BluetoothLEService.LocalBinder
+            appViewModel.updateServiceLiveData(binder.getService())
+
+        }
+        override fun onServiceDisconnected(clasName: ComponentName?) {
+            appViewModel.updateServiceLiveData(null)
         }
     }
 }
@@ -73,7 +107,7 @@ fun MainApp(appViewModel: AppViewModel) {
     val navigationState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val scope = rememberCoroutineScope()
     var selectedItemIndex by rememberSaveable {
-        mutableStateOf(0)
+        mutableIntStateOf(0)
     }
     val navController = rememberNavController()
     val bluetoothLEService = appViewModel.bluetoothLEServiceLiveData.observeAsState()
@@ -151,9 +185,11 @@ fun MainApp(appViewModel: AppViewModel) {
                     }
                 })
             }) {
-                NavHost(navController = navController, startDestination = "home", modifier = Modifier.padding(it)) {
+                NavHost(navController = navController, startDestination = "bluetooth", modifier = Modifier.padding(it)) {
                     composable("home") { HomeScreen()}
-                    composable("bluetooth") { BluetoothScreen()}
+                    composable("bluetooth") { bluetoothLEService.value?.let { service ->
+                        BluetoothScreen(service)
+                    }}
                     composable("nfc") { NFCScreen()}
                     composable("statistic") { StatisticScreen()}
                     composable("alert") { AlertScreen()}
