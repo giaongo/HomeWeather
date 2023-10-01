c7b9b5040fdb00a44fe71811f3e905ac249f442c
anish0123 <anishmhn@hotmail.com>
2023-10-01 10:48:24 +0300

    firebase cloud storage has been created

---
+++ HomeWeather/app/src/main/java/fi/metropolia/homeweather/dataclass/Temperature.kt
@@ -0,0 +1,12 @@
+package fi.metropolia.homeweather.dataclass
+
+
+import java.time.LocalDateTime
+
+
+data class Temperature(
+    var tempData: Float = 0.0F,
+    var date: String = LocalDateTime.now().toString(),
+) {
+
+}
