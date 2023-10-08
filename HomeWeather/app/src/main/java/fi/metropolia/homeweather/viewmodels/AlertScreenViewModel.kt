c867de2ca9000107dbc8e49c84383694dd72ece0
anish0123 <anishmhn@hotmail.com>
2023-10-08 03:35:05 +0300

    created option to add info to clarify the alert

--- HomeWeather/app/src/main/java/fi/metropolia/homeweather/viewmodels/AlertScreenViewModel.kt
+++ HomeWeather/app/src/main/java/fi/metropolia/homeweather/viewmodels/AlertScreenViewModel.kt
@@ -19,4 +19,25 @@ class AlertScreenViewModel: ViewModel() {
         }
     }
 
+    /**
+     * Method to refresh the data when info is added by user
+     */
+    suspend fun refresh () {
+        _alertData.value = AppRepository.getFirebaseData("alert", VoiceAlert::class.java)
+    }
+
+    /**
+     * Update data to the firebase
+     */
+    suspend fun updateFireBaseData (fieldName: String, documentId: String, collectionName: String, fieldData: Any) {
+        AppRepository.updateDocument(fieldName, documentId,collectionName, fieldData)
+    }
+
+    /**
+     * Method to get the documentId according to field data
+     */
+    suspend fun getDocumentId(fieldName: String, fieldData: Any, collectionName: String):String {
+        return AppRepository.getFireBaseDocumentId(fieldName, fieldData = fieldData, collectionName = collectionName)
+    }
+
 }
